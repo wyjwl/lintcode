aa92b977ab6e300e5e98462ee68abb66b8ae8339
yijwang <yijwang@thoughtworks.com>
2018-09-04 20:00:22 +0800

    lintcode 704

---
+++ 704flipLights.py
@@ -0,0 +1,18 @@
+#数学找规律
+
+def flipLights(n, m):
+
+    if m == 0:
+        return 1
+    if n == 1:
+        return 2
+    if n == 2 and m == 1:
+        return 3
+    if n == 2:
+        return 4
+    if m == 1:
+        return 4
+    if m == 2:
+        return 7
+
+    return 8
