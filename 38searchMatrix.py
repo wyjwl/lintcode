1a6f1fe623eb767ea22a7da79aa9e7c1c01b26c8
yijwang <yijwang@thoughtworks.com>
2018-09-08 23:21:59 +0800

    lintcode 38

---
+++ 38searchMatrix.py
@@ -0,0 +1,17 @@
+def searchMatrix(matrix, target):
+    if matrix == [] or matrix[0] == []:
+        return 0
+
+    row, column = len(matrix), len(matrix[0])
+    i, j = row - 1, 0
+    count = 0
+    while i >= 0 and j < column:
+        if matrix[i][j] == target:
+            count += 1
+            i -= 1
+            j += 1
+        elif matrix[i][j] < target:
+            j += 1
+        elif matrix[i][j] > target:
+            i -= 1
+    return count
